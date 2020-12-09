2a3174934b208991f1a30b8317845a81f9021bcb
Chienping Tsung <jianpingzong@outlook.com>
2020-12-09 15:58:11 +0800

    Update UNet.py

--- models/UNet.py
+++ models/UNet.py
@@ -3,21 +3,29 @@ from torch import nn
 from torch.nn import functional
 
 
-def two_conv(in_channels, out_channels, kernel_size=3):
+def double_conv(in_channels, out_channels, kernel_size=3):
     return nn.Sequential(
         nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size),
         nn.BatchNorm2d(out_channels),
-        nn.ReLU(),
+        nn.ReLU(inplace=True),
         nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size),
         nn.BatchNorm2d(out_channels),
-        nn.ReLU()
+        nn.ReLU(inplace=True)
     )
 
 
 def copy_and_crop(cont_map, expa_map):
-    gap = (cont_map.shape[2] - expa_map.shape[2]) // 2
-    cont_map = functional.pad(cont_map, [-gap, -gap, -gap, -gap])
-    return torch.cat((cont_map, expa_map), dim=1)
+    # The shape of cont_map and expa_map is (B, C, H, W).
+    _, _, cont_H, cont_W = cont_map.shape
+    _, _, expa_H, expa_W = expa_map.shape
+
+    diff_H = expa_H - cont_H
+    diff_W = expa_W - cont_W
+
+    cropped_map = functional.pad(cont_map, (diff_W // 2, diff_W - diff_W // 2,  # (padding_left, padding_right,
+                                            diff_H // 2, diff_H - diff_H // 2))  # padding_top, padding_bottom)
+
+    return torch.cat((expa_map, cropped_map), dim=1)  # dim=1 for channel
 
 
 class UNet(nn.Module):
@@ -30,27 +38,27 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
 
         # contracting path
-        self.cont1 = two_conv(in_channels, 64)
+        self.cont1 = double_conv(in_channels, 64)
         self.maxpool1 = nn.MaxPool2d(2)
-        self.cont2 = two_conv(64, 128)
+        self.cont2 = double_conv(64, 128)
         self.maxpool2 = nn.MaxPool2d(2)
-        self.cont3 = two_conv(128, 256)
+        self.cont3 = double_conv(128, 256)
         self.maxpool3 = nn.MaxPool2d(2)
-        self.cont4 = two_conv(256, 512)
+        self.cont4 = double_conv(256, 512)
         self.maxpool4 = nn.MaxPool2d(2)
 
         # bottleneck
-        self.bottleneck = two_conv(512, 1024)
+        self.bottleneck = double_conv(512, 1024)
 
         # expansive path
         self.up1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.expa1 = two_conv(1024, 512)
+        self.expa1 = double_conv(1024, 512)
         self.up2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.expa2 = two_conv(512, 256)
+        self.expa2 = double_conv(512, 256)
         self.up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.expa3 = two_conv(256, 128)
+        self.expa3 = double_conv(256, 128)
         self.up4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.expa4 = two_conv(128, 64)
+        self.expa4 = double_conv(128, 64)
 
         # output
         self.output = nn.Conv2d(64, out_channels, kernel_size=1)
@@ -92,27 +100,27 @@ class UNetM(nn.Module):
         super(UNetM, self).__init__()
 
         # contracting path
-        self.cont1 = two_conv(in_channels, 64)
+        self.cont1 = double_conv(in_channels, 64)
         self.maxpool1 = nn.MaxPool2d(2)
-        self.cont2 = two_conv(64, 128)
+        self.cont2 = double_conv(64, 128)
         self.maxpool2 = nn.MaxPool2d(2)
-        self.cont3 = two_conv(128, 256)
+        self.cont3 = double_conv(128, 256)
         self.maxpool3 = nn.MaxPool2d(2)
-        # self.cont4 = two_conv(256, 512)
+        # self.cont4 = double_conv(256, 512)
         # self.maxpool4 = nn.MaxPool2d(2)
 
         # bottleneck
-        self.bottleneck = two_conv(256, 512)
+        self.bottleneck = double_conv(256, 512)
 
         # expansive path
         # self.up1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        # self.expa1 = two_conv(1024, 512)
+        # self.expa1 = double_conv(1024, 512)
         self.up2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.expa2 = two_conv(512, 256)
+        self.expa2 = double_conv(512, 256)
         self.up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.expa3 = two_conv(256, 128)
+        self.expa3 = double_conv(256, 128)
         self.up4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.expa4 = two_conv(128, 64)
+        self.expa4 = double_conv(128, 64)
 
         # output
         self.output = nn.Conv2d(64, out_channels, kernel_size=1)
@@ -136,6 +144,68 @@ class UNetM(nn.Module):
         # x = self.expa1(copy_and_crop(x4, x))
         x = self.up2(x)
         x = self.expa2(copy_and_crop(x3, x))
+        x = self.up3(x)
+        x = self.expa3(copy_and_crop(x2, x))
+        x = self.up4(x)
+        x = self.expa4(copy_and_crop(x1, x))
+
+        return self.output(x)
+
+
+class UNetS(nn.Module):
+    """
+    Very simplified architecture described by Figure 1 of
+    https://arxiv.org/pdf/1505.04597.pdf
+    """
+
+    def __init__(self, in_channels, out_channels):
+        super(UNetS, self).__init__()
+
+        # contracting path
+        self.cont1 = double_conv(in_channels, 64)
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.cont2 = double_conv(64, 128)
+        self.maxpool2 = nn.MaxPool2d(2)
+        # self.cont3 = double_conv(128, 256)
+        # self.maxpool3 = nn.MaxPool2d(2)
+        # self.cont4 = double_conv(256, 512)
+        # self.maxpool4 = nn.MaxPool2d(2)
+
+        # bottleneck
+        self.bottleneck = double_conv(128, 256)
+
+        # expansive path
+        # self.up1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        # self.expa1 = double_conv(1024, 512)
+        # self.up2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        # self.expa2 = double_conv(512, 256)
+        self.up3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.expa3 = double_conv(256, 128)
+        self.up4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.expa4 = double_conv(128, 64)
+
+        # output
+        self.output = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # contracting path
+        x1 = self.cont1(x)
+        x = self.maxpool1(x1)
+        x2 = self.cont2(x)
+        x = self.maxpool2(x2)
+        # x3 = self.cont3(x)
+        # x = self.maxpool3(x3)
+        # x4 = self.cont4(x)
+        # x = self.maxpool4(x4)
+
+        # bottleneck
+        x = self.bottleneck(x)
+
+        # expansive path
+        # x = self.up1(x)
+        # x = self.expa1(copy_and_crop(x4, x))
+        # x = self.up2(x)
+        # x = self.expa2(copy_and_crop(x3, x))
         x = self.up3(x)
         x = self.expa3(copy_and_crop(x2, x))
         x = self.up4(x)
