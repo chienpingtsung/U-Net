1965e6ab69a1239d315b010c3430e8fd42953314
Chienping Tsung <jianpingzong@outlook.com>
2020-12-25 14:26:37 +0800

    Update evaluation.py

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,7 +1,7 @@
 from scipy import ndimage
 
 
-def calc_confusion_matrix(pred, gt, structure=None):
+def calc_confusion_matrix(pred, gt, structure):
     """
 
     :param pred: Predicted mask.
@@ -9,12 +9,8 @@ def calc_confusion_matrix(pred, gt, structure=None):
     :param structure: Structure for binary_dilation, None for not using tolerance.
     :return: (precision_TP, precision_TPFP, recall_TP, recall_TPFN)
     """
-    dilated_pred = pred
-    dilated_gt = gt
-
-    if structure:
-        dilated_pred = ndimage.binary_dilation(pred, structure=structure)
-        dilated_gt = ndimage.binary_dilation(gt, structure=structure)
+    dilated_pred = ndimage.binary_dilation(pred, structure=structure)
+    dilated_gt = ndimage.binary_dilation(gt, structure=structure)
 
     prec_TP = (pred & dilated_gt).sum()
     prec_TPFP = pred.sum()
