5313dcf3c7d34a140c7b44eb1210c120125fac6b
Chienping Tsung <jianpingzong@outlook.com>
2020-12-25 23:46:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import os
 from glob import glob
@@ -8,40 +9,88 @@ from PIL import Image
 from torch import nn
 from tqdm import tqdm
 
-from models.UNet import UNet
+from models.UNet import UNetM
 from toolbox.image import Tile
+from utils.evaluation import calc_confusion_matrix
 from utils.image import detile
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Now using {torch.cuda.device_count()} {device} divces.")
+logger = logging.getLogger(__name__)
 
-names = glob("ds/04v2crack/val/images/*.bmp")
 
-net = UNet(3, 1)
-if torch.cuda.device_count() > 1:
-    net = nn.DataParallel(net)
-net.to(device)
-net.load_state_dict(torch.load("U-Net.weights"))
-net.eval()
+def test(net, device, input_root, target_root, save_root=None):
+    assert os.path.exists(input_root), f"Path '{input_root}' doesn't exist."
+    assert os.path.exists(target_root), f"Path '{target_root}' doesn't exist."
 
-tile = Tile(572, 388)
+    if save_root and not os.path.exists(save_root):
+        os.makedirs(save_root)
 
-for name in tqdm(names):
-    with torch.no_grad():
-        im = Image.open(name).convert('RGB')
+    names = {os.path.basename(p)[:-4]  # -4 for file extension
+             for p in glob(os.path.join(input_root, '*.bmp'))}
 
-        width, height = im.size
-        lef = (math.ceil(width / 388) * 388 - width) // 2
-        top = (math.ceil(height / 388) * 388 - height) // 2
+    assert not names ^ {os.path.basename(p)[:-4]  # -4 for file extension
+                        for p in glob(os.path.join(target_root, '*.png'))}, \
+        "Missing file for matching between input and target."
 
-        im = np.array(im)
-        im = tile(im).transpose((0, 3, 1, 2))
-        im = torch.from_numpy(im).to(device, dtype=torch.float)
+    net.eval()
 
-        outputs = net(im)
-        outputs = torch.sigmoid(outputs)
-        predicted = outputs[:, 0, ...] * 255
-        predicted = np.uint8(predicted.cpu())
+    tile = Tile(572, 484)
 
-        ma = detile(predicted, (width, height), top, lef)
-        ma.save("ds/pred/{}.png".format(os.path.splitext(os.path.basename(name))[0]))
+    structure = np.ones((5, 5))
+    prec_TP = prec_TPFP = reca_TP = reca_TPFN = 0
+
+    tq = tqdm(names)
+    for name in tq:
+        with torch.no_grad():
+            im = Image.open(os.path.join(input_root, f'{name}.bmp')).convert('RGB')
+
+            width, height = im.size
+            left = (math.ceil(width / 484) * 484 - width) // 2
+            top = (math.ceil(height / 484) * 484 - height) // 2
+
+            im = np.array(im)
+            im = tile(im).transpose((0, 3, 1, 2))
+            im = torch.from_numpy(im).to(device, dtype=torch.float)
+
+            output = net(im)
+            output = torch.sigmoid(output)
+            pred = output[:, 0, ...] > 0.5
+            pred = np.uint8(pred.cpu()) * 255
+            pred = detile(pred, (width, height), top, left)
+
+            if save_root:
+                pred.save(os.path.join(save_root, f'{name}.png'))
+
+            gt = Image.open(os.path.join(target_root, f'{name}.png'))
+            pred = np.array(pred)
+            gt = np.array(gt)
+
+            p_TP, p_TPFP, r_TP, r_TPFN = calc_confusion_matrix(pred == 255, gt == 1, structure=structure)
+            prec_TP += p_TP
+            prec_TPFP += p_TPFP
+            reca_TP += r_TP
+            reca_TPFN += r_TPFN
+
+    infinitesimal = 1e-10
+    prec = prec_TP / (prec_TPFP + infinitesimal)
+    reca = reca_TP / (reca_TPFN + infinitesimal)
+    F1 = (2 * prec * reca) / (prec + reca + infinitesimal)
+
+    return prec, reca, F1
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logger.info(f'{torch.cuda.device_count()} cuda device available.')
+
+    net = UNetM(3, 1)
+    net.load_state_dict(torch.load('UNetM.pth'))
+    if torch.cuda.device_count() > 1:
+        net = nn.DataParallel(net)
+    net.to(device)
+
+    prec, reca, F1 = test(net,
+                          device,
+                          'ds/04v2crack/val/images/',
+                          'ds/04v2crack/val/labels/')
+
+    logger.info(f'Precision {prec}, recall {reca}, F1 {F1}.')
