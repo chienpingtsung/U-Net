5f9265378486ccb0734d4fb9b48c9422d33f72a7
Chienping Tsung <jianpingzong@outlook.com>
2020-12-27 13:20:02 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,33 +1,42 @@
+import logging
 from itertools import count
 
 import torch
 from torch import nn
+from torch.nn import BCEWithLogitsLoss
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from datasets.APD import APD202004v2crack
 from losses.FocalLoss import FocalLoss
-from models.UNet import UNet
+from models.WNet import WNet
+
+logger = logging.getLogger(__name__)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Now using {torch.cuda.device_count()} {device} divces.")
 
-BATCH_size = 16
+batch_size = 16
 
-trainset = APD202004v2crack("ds/04v2crack_tile_572_388/train/")
-testset = APD202004v2crack("ds/04v2crack_tile_572_388/val/")
+trainset = APD202004v2crack("ds/04v2crack_edge_tile_572_484/train/")
+testset = APD202004v2crack("ds/04v2crack_edge_tile_572_484/val/")
 
-trainloader = DataLoader(trainset, batch_size=BATCH_size, shuffle=True)
-testloader = DataLoader(testset, batch_size=BATCH_size, shuffle=False)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 
-net = UNet(3, 1)
+net = WNet(3, 1)
+net.use_seg_net = False
+net.use_edge_net = True
+net.requires_grad_for_layers(True, prefix='edge')
+net.requires_grad_for_layers(False, prefix='seg')
 if torch.cuda.device_count() > 1:
     net = nn.DataParallel(net)
 net.to(device)
 
 criterion = FocalLoss()
-optimizer = torch.optim.Adam(net.parameters(), weight_decay=1e-4)
+test_crit = BCEWithLogitsLoss()
+optimizer = torch.optim.Adam([param for _, param in net.named_parameters('edge')])
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=5)
 
 min_loss = 1000
@@ -36,6 +45,8 @@ write = SummaryWriter()
 
 for epoch in count():
     net.train()
+    train_loss = 0.0
+    train_times = 0
     tq = tqdm(trainloader)
     for data in tq:
         inputs = data['image'].to(device, dtype=torch.float)
@@ -44,6 +55,9 @@ for epoch in count():
 
         outputs = net(inputs)
         loss = criterion(outputs, labels.unsqueeze(dim=1))
+        train_loss += loss.item()
+        train_times += 1
+
         loss.backward()
         optimizer.step()
 
@@ -60,7 +74,7 @@ for epoch in count():
             labels = data['mask'].to(device, dtype=torch.float) // 255
 
             outputs = net(inputs)
-            loss = criterion(outputs, labels.unsqueeze(dim=1))
+            loss = test_crit(outputs, labels.unsqueeze(dim=1))
             test_loss += loss.item()
             test_times += 1
 
@@ -68,12 +82,13 @@ for epoch in count():
 
     test_loss /= test_times
     write.add_scalar("Loss/test", test_loss, epoch)
-    print("Epoch {:3} test_loss: {}".format(epoch, test_loss))
+    logger.info("Epoch {:3} test_loss: {}".format(epoch, test_loss))
+
+    torch.save(net.module.state_dict(), f'WNet{epoch}.pth')
 
     if test_loss < min_loss:
         min_loss = test_loss
         min_loss_epoch = epoch
-        torch.save(net.state_dict(), "U-Net.weights")
 
     if epoch - min_loss_epoch > 10:
         break
