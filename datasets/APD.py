21d53a208b814c01f05fa1eb58ec6a1ad92689a7
Chienping Tsung <jianpingzong@outlook.com>
2020-12-30 13:51:07 +0800

    Update APD.py

--- datasets/APD.py
+++ datasets/APD.py
@@ -1,56 +1,50 @@
 import logging
-import os
 from glob import glob
+from pathlib import Path
 
-import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
 
 
-class APD202004v2crack(Dataset):
-    """
-    Cracks from APD202004v2 dataset.
-    """
-
-    def __init__(self, path):
-        """You should organize your dataset folder as follow, and match each basename of images and labels.
-         .
-         ├── images
-         │   ├── image0.bmp
-         │   ├── image1.bmp
-         │   ├── image2.bmp
-         │   └── ...
-         └── labels
-             ├── image0.png
-             ├── image1.png
-             ├── image2.png
-             └── ...
-
-        :param path: Path to the dataset.
-        :param transform: Transform applied to data.
+class ImageFolder(Dataset):
+    def __init__(self, root, transform=None):
         """
-        super(APD202004v2crack, self).__init__()
+        The directory should be organized as following tree, and match each stem of images and labels.
+        .
+        ├── images
+        │   ├── image0.bmp
+        │   ├── image1.bmp
+        │   ├── image2.bmp
+        │   └── ...
+        └── labels
+            ├── image0.png
+            ├── image1.png
+            ├── image2.png
+            └── ...
 
-        self.image_path = os.path.join(path, 'images/')
-        self.label_path = os.path.join(path, 'labels/')
+        :param root: Path to the dataset.
+        :param transform: Transforms for data augmentation.
+        """
+        super(ImageFolder, self).__init__()
 
-        self.names = [
-            os.path.splitext(os.path.basename(p))[0]
-            for p in glob(os.path.join(self.image_path, '*.bmp'))
-        ]
+        self.image_path = Path(root).joinpath('images/')
+        self.label_path = Path(root).joinpath('labels/')
+
+        self.stems = [Path(p).stem
+                      for p in glob(self.image_path.joinpath('*.bmp'))]
+
+        self.transform = transform
 
     def __len__(self):
-        return len(self.names)
+        return len(self.stems)
 
     def __getitem__(self, item):
-        image = Image.open(os.path.join(self.image_path, '{}.bmp'.format(self.names[item])))
-        mask = Image.open(os.path.join(self.label_path, '{}.png'.format(self.names[item])))
+        image = Image.open(self.image_path.joinpath(f'{self.stems[item]}.bmp'))
+        label = Image.open(self.label_path.joinpath(f'{self.stems[item]}.png'))
 
-        sample = {
-            'image': np.array(image).transpose((2, 0, 1)),
-            'mask': np.array(mask)
-        }
+        if self.transform:
+            image, label = self.transform(image, label)
 
-        return sample
+        return image, label
