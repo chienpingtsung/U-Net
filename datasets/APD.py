c3b938c8f6a594faf03726fea557a7168af9cb8c
Chienping Tsung <jianpingzong@outlook.com>
2021-01-15 16:06:49 +0800

    Update APD.py

--- datasets/APD.py
+++ datasets/APD.py
@@ -1,5 +1,4 @@
 import logging
-from glob import glob
 from pathlib import Path
 
 from PIL import Image
@@ -48,3 +47,30 @@ class ImageFolder(Dataset):
             image, label = self.transform(image, label)
 
         return image, label
+
+
+class WNetDataset(Dataset):
+    def __init__(self, images_root, seg_labels_root, edge_labels_root, transform=None):
+        super(WNetDataset, self).__init__()
+
+        self.images_root = Path(images_root)
+        self.seg_labels_root = Path(seg_labels_root)
+        self.edge_labels_root = Path(edge_labels_root)
+
+        self.stems = [p.stem
+                      for p in self.images_root.glob('*.bmp')]
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.stems)
+
+    def __getitem__(self, item):
+        image = Image.open(self.images_root.joinpath(f'{self.stems[item]}.bmp'))
+        seg_label = Image.open(self.seg_labels_root.joinpath(f'{self.stems[item]}.png'))
+        edge_label = Image.open(self.edge_labels_root.joinpath(f'{self.stems[item]}.png'))
+
+        if self.transform:
+            image, seg_label, edge_label = self.transform(image, seg_label, edge_label)
+
+        return image, seg_label, edge_label
