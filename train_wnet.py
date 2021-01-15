1b21a26c558c2d3dc3c8575c87bd22fba80f418c
Chienping Tsung <jianpingzong@outlook.com>
2021-01-15 16:34:24 +0800

    Update train_wnet.py

--- train_wnet.py
+++ train_wnet.py
@@ -8,9 +8,9 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from datasets.APD import ImageFolder
+from datasets.APD import WNetDataset
 from losses.FocalLoss import FocalLoss
-from models.WNet import WNet
+from models.WNet import WNet, WNetSIM
 from test import test
 from transforms.transforms import PILToTensor
 
@@ -21,23 +21,22 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 logger.info(f'{torch.cuda.device_count()} cuda devices available.')
 logger.info(f'Using {device} device.')
 
-batch_size = 16
+batch_size = 3 * torch.cuda.device_count()
 
-trainset = ImageFolder('data/BSDS500512/train/', transform=PILToTensor())
+trainset = WNetDataset('../JupyterLab/datasets/07v2crack_512/train/images/',
+                       '../JupyterLab/datasets/07v2crack_512/train/labels/',
+                       '../JupyterLab/datasets/07v2crack_edge_512/train/labels/',
+                       transform=PILToTensor())
 trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-net = WNet(3, 1)
-net.use_seg_net = False
-net.use_edge_net = True
-net.requires_grad_for_layers(False, 'seg')
-net.requires_grad_for_layers(True, 'edge')
+net = WNetSIM(3, 1)
 if torch.cuda.device_count() > 1:
     net = nn.DataParallel(net)
 net.to(device)
 
 criterion = FocalLoss()
 optimizer = torch.optim.Adam(net.parameters())
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', patience=5, verbose=True)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=3, verbose=True)
 
 best_F1 = 0
 best_F1_epoch = 0
@@ -45,39 +44,36 @@ writer = SummaryWriter()
 
 for epoch in count():
     net.train()
-    train_loss = 0
-    train_time = 0
     tq = tqdm(trainloader)
     for data in tq:
-        images, labels = data
+        images, seg_labels, edge_labels = data
         images = images.to(device, dtype=torch.float)
-        labels = labels.to(device, dtype=torch.float) // 255
+        seg_labels = seg_labels.to(device, dtype=torch.float) // 255
+        edge_labels = edge_labels.to(device, dtype=torch.float) // 255
 
-        output = net(images)
-        loss = criterion(output, labels)
-        train_loss += loss.item()
-        train_time += 1
+        seg_output, edge_output = net(images)
+        seg_loss = criterion(seg_output, seg_labels)
+        edge_loss = criterion(edge_output, edge_labels)
 
         optimizer.zero_grad()
-        loss.backward()
+        (seg_loss + edge_loss).backward()
         optimizer.step()
 
-        writer.add_scalar('Loss/train', loss.item(), epoch)
-        tq.set_description(f'Training epoch {epoch:3}, loss {loss.item()}')
-
-    scheduler.step(train_loss / train_time)
+        writer.add_scalar('Loss/seg', seg_loss.item(), epoch)
+        writer.add_scalar('Loss/edge', edge_loss.item(), epoch)
+        tq.set_description(f'Training epoch {epoch:3}, seg_loss {seg_loss.item()}, edge_loss {edge_loss.item()}')
 
     prec, reca, F1 = test(net, device,
-                          'data/BSDS500512/test/images/',
-                          'data/BSDS500512/test/labels/',
-                          f'data/BSDS500test/{epoch}/')
+                          '../JupyterLab/datasets/07v2crack/val/images/',
+                          '../JupyterLab/datasets/07v2crack/val/labels/',
+                          Path(writer.log_dir).joinpath(f'test/{epoch}/'))
 
     writer.add_scalar('Precision/test', prec, epoch)
     writer.add_scalar('Recall/test', reca, epoch)
     writer.add_scalar('F1/test', F1, epoch)
     logger.info(f'Epoch {epoch}, precision {prec}, recall {reca}, F1 {F1}.')
 
-    torch.save(net.module.state_dict(), Path(writer.log_dir).joinpath(f'WNet{epoch}.pth'))
+    torch.save(net.module.state_dict(), Path(writer.log_dir).joinpath(f'WNetSIM{epoch}.pth'))
 
     if F1 > best_F1:
         best_F1 = F1
@@ -85,5 +81,7 @@ for epoch in count():
 
     if epoch - best_F1_epoch > 10:
         break
+
+    scheduler.step(F1)
 
 logger.info(f'Best at epoch {best_F1_epoch}, F1 is {best_F1}.')
